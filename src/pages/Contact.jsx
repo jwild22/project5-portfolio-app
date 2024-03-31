dd09f9630d033ab22d1f366c0b8b5bf18045b849
Farukh K <lasamasa24@gmail.com>
2024-03-31 20:54:24 +0500

    for submission

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -16,19 +16,19 @@ export default function Contact() {
               type="text"
               name="name"
               placeholder="Enter your name"
-              className="p-2 bg-transparent border-2 rounded-md w-80 focus:outline-none"
+              className="p-2 bg-transparent border-2 rounded-md w-96 focus:outline-none"
             />
             <input
               type="text"
               name="email"
               placeholder="Enter your email"
-              className="p-2 bg-transparent border-2 rounded-md w-80 focus:outline-none"
+              className="p-2 bg-transparent border-2 rounded-md w-96 focus:outline-none"
             />
             <textarea
               name="message"
               placeholder="Enter your message"
               rows="10"
-              className="p-2 bg-transparent border-2 rounded-md w-80 focus:outline-none"
+              className="p-2 bg-transparent border-2 rounded-md w-96 focus:outline-none"
             ></textarea>
             <button className="text-white bg-gradient-to-b from-cyan-600 to-teal-600 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
               Submit
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,7 +1,31 @@
 export default function Experience() {
   return (
-    <div className="flex items-center md:h-screen justify-center">
+    <div className="flex flex-col items-center md:h-screen justify-center gap-6">
       <h3 className="text-5xl font-medium inline">Experience</h3>
+      <div className="w-11/12 md:w-10/12 mt-12 relative before:absolute before:top-0 before:left-16 before:rounded-full before:bottom-10 sm:before:bottom-2 before:w-1 before:bg-black">
+        <div className="pl-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-teal-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
+          <h3 className="absolute left-0 text-lg font-semibold">2017</h3>
+          <p>Graduated Kimep University with a major Account and Audit.</p>
+        </div>
+        <div className="pl-24 mt-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-teal-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
+          <h3 className="absolute left-0 text-lg font-semibold">2021</h3>
+          <p>
+            Ernst&Yung Internship. Worked as a consultant in the field of audit.
+          </p>
+        </div>
+        <div className="pl-24 mt-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-teal-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
+          <h3 className="absolute left-0 text-lg font-semibold">2021</h3>
+          <p>JTI Financial Analyst. Worked as a financial analyst.</p>
+        </div>
+        <div className="pl-24 mt-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-teal-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
+          <h3 className="absolute left-0 text-lg font-semibold">2023</h3>
+          <p>PepsiCo Senior Financial Analyst. Worked as a financial analyst.</p>
+        </div>
+        <div className="pl-24 mt-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-teal-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
+          <h3 className="absolute left-0 text-lg font-semibold">2024</h3>
+          <p>PMI Financial Analyst. Worked as a financial analyst.</p>
+        </div>
+      </div>
     </div>
   );
 }
